09991473cf7d668defd21a583900e50c1d10566f
Richard Huang <rzhuangsg115@gmail.com>
2021-07-19 18:11:23 -0500

    Can grabe decks based on current user

--- app/api/deck_routes.py
+++ app/api/deck_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
+from flask_migrate import current
 from app.models import Deck, Category
 
 deck_routes = Blueprint('decks', __name__)
@@ -14,9 +15,18 @@ def decks(id):
 
 
 # Grab decks based on categories
-@deck_routes.route("/<string:category>")
+@deck_routes.route("/category/<string:category>")
 def cat_decks(category):
     found_category = Category.query.filter_by(name=f"{category}").first()
     decks = Deck.query.filter_by(categoryId=f"{found_category.id}").all()
 
+    return {"decks": [deck.to_dict() for deck in decks]}
+
+
+# Grab decks based on current user
+@deck_routes.route("/user/<int:id>")
+@login_required
+def user_decks(id):
+    decks = Deck.query.filter_by(userId=f"{current_user.id}").all()
+    
     return {"decks": [deck.to_dict() for deck in decks]}
