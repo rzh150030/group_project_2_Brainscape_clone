528168c0e5fa8ed3810ae0eb810a6a55315f6961
Richard Huang <rzhuangsg115@gmail.com>
2021-07-19 16:58:05 -0500

    Change 3rd seed deck to be more recognizable

--- app/seeds/decks.py
+++ app/seeds/decks.py
@@ -4,7 +4,7 @@ from app.models import db, Deck
 def seed_decks():
     deck1 = Deck(title="Deck 1", categoryId=1, userId=1)
     deck2 = Deck(title="Deck 2", categoryId=1, userId=1)
-    deck3 = Deck(title="Deck 3", categoryId=2, userId=2)
+    deck3 = Deck(title="Deck NO", categoryId=2, userId=2)
 
     db.session.add(deck1)
     db.session.add(deck2)
