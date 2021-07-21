b2122f81473d08c23d6ab5c446fa608c89317a66
Richard Huang <rzhuangsg115@gmail.com>
2021-07-21 18:07:13 -0500

    remove verification for login

--- app/api/category_routes.py
+++ app/api/category_routes.py
@@ -5,17 +5,6 @@ from app.models import db, Category
 category_routes = Blueprint('categories', __name__)
 
 
-def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f'{field} : {error}')
-    return errorMessages
-
-
 @category_routes.route("/")
 def get_categories():
     categories = Category.query.all()
