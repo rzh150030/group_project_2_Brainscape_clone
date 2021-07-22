a38ce518e6beb638256e501b00b4ae7df7582f27
Gustavo <g.ebenezer.miranda@gmail.com>
2021-07-22 16:41:28 -0700

    simple question and answer div are working

--- react-app/src/components/DeckMainHeader/DeckMainHeader.js
+++ react-app/src/components/DeckMainHeader/DeckMainHeader.js
@@ -1,7 +1,10 @@
 import './DeckMainHeader.css'
 import logo from '../../images/logo.png'
+import { Link, useParams } from 'react-router-dom';
 
 const DeckMainHeader = () => {
+  const { deckId } = useParams();
+
   return (
     <div id="deck-main-header-div">
       <div id="language-logo-div">
@@ -11,7 +14,9 @@ const DeckMainHeader = () => {
       <h1>Python</h1>
     </div>
     <div id="study-deck-button-div">
-      <button>Study Deck</button>
+      <Link to={`/study-deck-page/${deckId}`}>
+        <button>Study Deck</button>
+      </Link>
     </div>
     <div id="quote-div">
       <h1>Quote</h1>
--- react-app/src/components/StudyDeckPage/StudyDeckPage.js
+++ react-app/src/components/StudyDeckPage/StudyDeckPage.js
@@ -1,19 +1,64 @@
 import './StudyDeckPage.css'
 import SideBar from '../SideBar/SideBar'
 import DeckMainHeader from '../DeckMainHeader/DeckMainHeader'
+import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 
 
 const StudyDeckPage = () => {
   const page = 'study-deck-page';
+  const currentDeck = useSelector((state) => state.currentDeck)
+  const [currentCard, setCurrentCard] = useState(0)
 
+  const [cardState, setCardState] = useState('question')
+
+  if (currentDeck.length === 0) {
+    return <Redirect to="/decks-page" />
+  }
+
+  const questionText = currentDeck[currentCard].question;
+  const answerText = currentDeck[currentCard].answer;
+
+  const handleClick = () => {
+    if (cardState === "question") setCardState("answer")
+    if (cardState === "answer") setCardState("question")
+  }
+
+  // useEffect(() => {
+
+  // }, [cardState])
 
   return (
     <div id="study-page-outer-main-div">
       <SideBar page={page} />
       <DeckMainHeader />
+
+
       <div id="study-card-container">
-        <h1>Testing</h1>
+        {cardState === "question" ?
+        <>
+          <div className="study-card-div">
+          <div>{questionText}</div>
+        </div>
+
+        <div id="study-card-reveal-answer-button-div">
+          <button id="study-card-reveal-answer-button" onClick={handleClick}>View Answer</button>
+        </div>
+        </>
+        :
+        <>
+          <div className="study-card-div">
+          <div>{answerText}</div>
+        </div>
+
+        <div id="study-card-reveal-question-button-div">
+          <button id="study-card-reveal-question-button" onClick={handleClick}>View Question</button>
+        </div>
+        </>
+        }
       </div>
+
     </div>
   );
 }
