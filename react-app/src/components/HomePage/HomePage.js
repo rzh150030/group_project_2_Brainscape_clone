456ae599eb5a4aafb30570986e203e205827fdf7
John Shivers <johnshivers3@gmail.com>
2021-07-23 06:13:52 -0400

    forms and homepage styling

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -5,15 +5,15 @@ import * as sessionActions from "../../store/session";
 import background1 from "../../images/pexels-vlada-karpovich-4050296.jpg";
 import background2 from "../../images/pexels-christina-morillo-1181676.jpg";
 // import background3 from "../../images/pexels-pexels-pixabay-270408.jpg";
-import background4 from "../../images/pexels-olia-danilevich-4974920.jpg";
-import background5 from "../../images/programmer-5863772.jpg";
-import background6 from "../../images/pexels-cottonbro-4709291.jpg";
-import background7 from "../../images/pexels-cottonbro-5474032.jpg";
-import background8 from "../../images/pexels-ketut-subiyanto-4474038.jpg";
-import background9 from "../../images/pexels-mart-production-7605984.jpg";
-import background10 from "../../images/pexels-mikhail-nilov-7988240.jpg";
-import background11 from "../../images/pexels-startup-stock-photos-7374.jpg";
-import background12 from "../../images/pexels-vlada-karpovich-4050350.jpg";
+import background3 from "../../images/pexels-olia-danilevich-4974920.jpg";
+import background4 from "../../images/programmer-5863772.jpg";
+import background5 from "../../images/pexels-cottonbro-4709291.jpg";
+import background6 from "../../images/pexels-cottonbro-5474032.jpg";
+import background7 from "../../images/pexels-ketut-subiyanto-4474038.jpg";
+import background8 from "../../images/pexels-mart-production-7605984.jpg";
+import background9 from "../../images/pexels-mikhail-nilov-7988240.jpg";
+import background10 from "../../images/pexels-startup-stock-photos-7374.jpg";
+import background11 from "../../images/pexels-vlada-karpovich-4050350.jpg";
 
 import Logo from "../Logo/Logo";
 import "./HomePage.css";
@@ -22,7 +22,7 @@ const HomePage = () => {
   const backgroundsArr = [
     background1,
     background2,
-    // background3,
+    background3,
     background4,
     background5,
     background6,
@@ -31,7 +31,6 @@ const HomePage = () => {
     background9,
     background10,
     background11,
-    background12,
   ];
   const [bgImage, setBgImage] = useState({ url: backgroundsArr[0], index: 0 });
   const [showing, setShowing] = useState(true);
@@ -39,13 +38,13 @@ const HomePage = () => {
 
   useEffect(() => {
     const bgInterval = setInterval(() => {
-      if (bgImage.index < 11)
+      if (bgImage.index < 10)
         setBgImage({
           url: backgroundsArr[bgImage.index + 1],
           index: bgImage.index + 1,
         });
       else setBgImage({ url: backgroundsArr[0], index: 0 });
-    }, 7000);
+    }, 3000);
     return () => {
       clearInterval(bgInterval);
     };
