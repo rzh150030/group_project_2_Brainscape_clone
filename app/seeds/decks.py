d42d7f0ab894c44a0bb7526931f5cc0d0eae1b15
Richard Huang <rzhuangsg115@gmail.com>
2021-07-24 14:06:39 -0500

    add new seed deck

--- app/seeds/decks.py
+++ app/seeds/decks.py
@@ -5,11 +5,12 @@ def seed_decks():
     deck1 = Deck(title="Java Questions", categoryId=1, userId=1)
     deck2 = Deck(title="More Java Questions", categoryId=1, userId=1)
     deck3 = Deck(title="Python Questions", categoryId=2, userId=2)
-    deck2 = Deck(title="Docker Questions", categoryId=5, userId=1)
+    deck4 = Deck(title="Docker Questions", categoryId=5, userId=1)
 
     db.session.add(deck1)
     db.session.add(deck2)
     db.session.add(deck3)
+    db.session.add(deck4)
 
     db.session.commit()
 
