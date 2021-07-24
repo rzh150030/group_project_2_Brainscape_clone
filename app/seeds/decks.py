c8fe26c18371045f4506398bd3387ed1ca628eff
Richard Huang <rzhuangsg115@gmail.com>
2021-07-24 14:05:46 -0500

    change some deck seeds

--- app/seeds/decks.py
+++ app/seeds/decks.py
@@ -2,9 +2,10 @@ from app.models import db, Deck
 
 
 def seed_decks():
-    deck1 = Deck(title="Deck 1", categoryId=1, userId=1)
-    deck2 = Deck(title="Deck 2", categoryId=1, userId=1)
-    deck3 = Deck(title="Deck NO", categoryId=2, userId=2)
+    deck1 = Deck(title="Java Questions", categoryId=1, userId=1)
+    deck2 = Deck(title="More Java Questions", categoryId=1, userId=1)
+    deck3 = Deck(title="Python Questions", categoryId=2, userId=2)
+    deck2 = Deck(title="Docker Questions", categoryId=5, userId=1)
 
     db.session.add(deck1)
     db.session.add(deck2)
