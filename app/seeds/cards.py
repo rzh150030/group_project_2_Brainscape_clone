8453a1c1285a9696ada8d01258c570d6322305e0
Richard Huang <rzhuangsg115@gmail.com>
2021-07-25 10:55:58 -0500

    add all Java cards

--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -7,10 +7,18 @@ def seed_cards():
     Jcard3 = Card(question="What is the default session time in PHP?", answer='The default session time in php is until the closing of the browser', deckId=1)
     Jcard4 = Card(question="Intensity of a color can be described through", answer='Hue', deckId=1)
     Jcard5 = Card(question="Which cryptographic extension provides generation and verification of digital signatures?", answer='PHP-CloseSSL', deckId=1)
+    Jcard6 = Card(question="What does the unlink() function do?", answer='The unlink() function is dedicated for file system handling. It simply deletes the file given as entry.', deckId=2)
+    Jcard7 = Card(question="Which of the following variables is not a predefined variable?", answer='$get', deckId=2)
+    Jcard8 = Card(question="How do you call a function named 'myFunction'?", answer='myFunction()', deckId=2)
 
-    db.session.add(card1)
-    db.session.add(card2)
-    db.session.add(card3)
+    db.session.add(Jcard1)
+    db.session.add(Jcard2)
+    db.session.add(Jcard3)
+    db.session.add(Jcard4)
+    db.session.add(Jcard5)
+    db.session.add(Jcard6)
+    db.session.add(Jcard7)
+    db.session.add(Jcard8)
 
     db.session.commit()
 
