9ec78a30a65dd399a0e85fba4657812fc33ee4fb
John Shivers <johnshivers3@gmail.com>
2021-07-25 12:39:51 -0400

    deletebtn

--- react-app/src/components/DeckMainHeader/DeckMainHeader.js
+++ react-app/src/components/DeckMainHeader/DeckMainHeader.js
@@ -9,6 +9,7 @@ import * as deckActions from "../../store/decks";
 const DeckMainHeader = () => {
   const cards = useSelector((state) => state.cards);
   const category = useSelector((state) => state.categories.category);
+  const currentDeck = useSelector((state) => state.currentDeck);
   const dispatch = useDispatch();
   const { deckId } = useParams();
   const history = useHistory();
@@ -18,6 +19,7 @@ const DeckMainHeader = () => {
     dispatch(categoryActions.getCategory(deckId));
   }, [cards]);
 
+  useEffect(()=>{},[currentDeck])
   const deleteUserDeck = (deleteId, userId) => {
     dispatch(deckActions.removeDeck({ deckId: deleteId, userId }));
     history.push("/decks-page");
@@ -32,9 +34,11 @@ const DeckMainHeader = () => {
         <h1>{category?.name ? category.name : "Select a deck"}</h1>
       </div>
       <div id="study-deck-button-div">
-        <Link to={`/study-deck-page/${deckId}`}>
-          <button className="nav-button study-button">Study Deck</button>
-        </Link>
+        {cards ? (
+          <Link to={`/study-deck-page/${deckId}`}>
+            <button className="nav-button study-button">Study Deck</button>
+          </Link>
+        ) : null}
       </div>
       <div id="quote-div">
         <h1>Quote</h1>
