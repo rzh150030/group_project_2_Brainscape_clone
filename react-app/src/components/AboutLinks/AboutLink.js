51f145599a09d52e692632b87f224fed2babd0e7
Richard Huang <rzhuangsg115@gmail.com>
2021-08-24 14:07:08 -0500

    add about links

--- react-app/src/components/AboutLinks/AboutLink.js
+++ react-app/src/components/AboutLinks/AboutLink.js
@@ -1,11 +1,16 @@
 import "./AboutLink.css";
 
-
 const AboutLink = () => {
+
 
     return (
         <footer>
-            <p>FFFFFF</p>
+            <p>Developed by:
+                <a href="https://github.com/rzh150030">Richard Huang</a>
+                <a href="https://github.com/justin-by">Justin Sung</a>
+                <a href="https://github.com/johnshivers3">John Shivers</a>
+                <a href="https://github.com/goosey-goose">Gustavo Miranda</a>
+            </p>
         </footer>
     );
 };
