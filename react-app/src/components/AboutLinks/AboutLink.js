4f2ac00166978b766fd23a701cda32b3c49d76a7
Richard Huang <rzhuangsg115@gmail.com>
2021-08-24 14:16:41 -0500

    start styling footer

--- react-app/src/components/AboutLinks/AboutLink.js
+++ react-app/src/components/AboutLinks/AboutLink.js
@@ -4,12 +4,12 @@ const AboutLink = () => {
 
 
     return (
-        <footer>
-            <p>Developed by:
-                <a href="https://github.com/rzh150030">Richard Huang</a>
-                <a href="https://github.com/justin-by">Justin Sung</a>
-                <a href="https://github.com/johnshivers3">John Shivers</a>
-                <a href="https://github.com/goosey-goose">Gustavo Miranda</a>
+        <footer className="about-links">
+            <p className="author-credits">Developed by:
+                <a className="authors" href="https://github.com/rzh150030">Richard Huang</a>
+                <a className="authors" href="https://github.com/justin-by">Justin Sung</a>
+                <a className="authors" href="https://github.com/johnshivers3">John Shivers</a>
+                <a className="authors" href="https://github.com/goosey-goose">Gustavo Miranda</a>
             </p>
         </footer>
     );
