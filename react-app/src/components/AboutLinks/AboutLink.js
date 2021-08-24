082efabe23a2fed584c8ce67b2e15d819ddbe884
Richard Huang <rzhuangsg115@gmail.com>
2021-08-24 15:09:14 -0500

    gave underline to my name

--- react-app/src/components/AboutLinks/AboutLink.js
+++ react-app/src/components/AboutLinks/AboutLink.js
@@ -6,10 +6,16 @@ const AboutLink = () => {
     return (
         <footer className="about-links">
             <p className="author-credits">Developed by:
-                <a className="authors" href="https://github.com/rzh150030">Richard Huang</a>
+                <a className="authors" href="https://github.com/rzh150030">Richard Huang
+                    <div className="underline"></div>
+                </a>
                 <a className="authors" href="https://github.com/justin-by">Justin Sung</a>
                 <a className="authors" href="https://github.com/johnshivers3">John Shivers</a>
                 <a className="authors" href="https://github.com/goosey-goose">Gustavo Miranda</a>
+                <a hidden className="authors" href="https://github.com/rzh150030">Richard Huang</a>
+                <a hidden className="authors" href="https://github.com/justin-by">Justin Sung</a>
+                <a hidden className="authors" href="https://github.com/johnshivers3">John Shivers</a>
+                <a hidden className="authors" href="https://github.com/goosey-goose">Gustavo Miranda</a>
             </p>
         </footer>
     );
