cb4c089a46c3e41608930c9ee0c1de4e653c179f
Richard Huang <rzhuangsg115@gmail.com>
2021-08-27 17:00:19 -0500

    create reducer and case for getting decks

--- react-app/src/store/search.js
+++ react-app/src/store/search.js
@@ -1,1 +1,13 @@
 const GET_DECKS = "search/GET_DECKS";
+
+
+const initialState = {results: []};
+
+export default function reducer(state = initialState, action) {
+    switch(action.type) {
+        case GET_DECKS:
+            return {results: action.payload};
+        default:
+            return state;
+    }
+}
