1368f2ba376aae95d5020340660e35ea6df43fc4
Richard Huang <rzhuangsg115@gmail.com>
2021-08-27 16:20:31 -0500

    made search case insensitive

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -7,5 +7,5 @@ search_routes = Blueprint('search', __name__)
 # Search for decks by title
 @search_routes.route("/<string:name>")
 def search_decks(name):
-    decks = Deck.query.filter(Deck.title.like("%" + name + "%")).all()
+    decks = Deck.query.filter(Deck.title.ilike("%" + name + "%")).all()
     return {"results": [deck.to_dict() for deck in decks]}
