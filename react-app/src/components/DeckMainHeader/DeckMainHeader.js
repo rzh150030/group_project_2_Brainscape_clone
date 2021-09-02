0185fadabff16df7a4c51058f6527b9228bbfa22
Richard Huang <rzhuangsg115@gmail.com>
2021-09-02 16:49:51 -0500

    reset displays when user deletes deck

--- react-app/src/components/DeckMainHeader/DeckMainHeader.js
+++ react-app/src/components/DeckMainHeader/DeckMainHeader.js
@@ -8,6 +8,7 @@ import LoginForm from "../auth/LoginForm";
 import SignUpForm from "../auth/SignUpForm";
 import * as categoryActions from "../../store/categories";
 import * as deckActions from "../../store/decks";
+import * as cardActions from "../../store/cards";
 
 const DeckMainHeader = () => {
   const cards = useSelector((state) => state.cards);
@@ -29,6 +30,8 @@ const DeckMainHeader = () => {
 
   const deleteUserDeck = (deleteId, userId) => {
     dispatch(deckActions.removeDeck({ deckId: deleteId, userId }));
+    dispatch(categoryActions.resetCategoryAction());
+    dispatch(cardActions.wipeCurrentCards());
     history.push("/decks-page");
   };
 
