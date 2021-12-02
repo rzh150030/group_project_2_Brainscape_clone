f9d39ebdaf754dcc368eda4106ef6b7ae0302689
rzh150030 <rzhuangsg115@gmail.com>
2021-12-02 14:44:19 -0600

    adding condition to check if user owns deck

--- react-app/src/components/SideBar/SideBar.js
+++ react-app/src/components/SideBar/SideBar.js
@@ -4,18 +4,26 @@ import MyDecksSBTitle from '../MyDecksSBTitle/MyDecksSBTitle'
 import AddNewCard from '../AddNewCard/AddNewCard';
 import { useParams } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const SideBar = ({page}) => {
-
+  const userOwnsDeck = false;
   const { deckId } = useParams();
   const userDecks = useSelector(state => state.userDecks.decks);
-  
+
+  useEffect(() => {
+    for (let deck of userDecks) {
+      if (deck.id === deckId) {
+        userOwnsDeck = true;
+      }
+    }
+  }, [])
 
   return (
     <div id="sidebar-div">
       <ProfileLogo />
       <MyDecksSBTitle page={page}/>
-      {deckId && <AddNewCard deckId={deckId}/>}
+      {deckId && userOwnsDeck && <AddNewCard deckId={deckId}/>}
     </div>
   );
 }
