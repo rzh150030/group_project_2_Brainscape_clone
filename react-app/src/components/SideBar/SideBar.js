f5ecc129a562d050f6c3fe2f72e080288e19a91d
rzh150030 <rzhuangsg115@gmail.com>
2021-12-02 14:30:04 -0600

    user deck extracted from store in sidebar

--- react-app/src/components/SideBar/SideBar.js
+++ react-app/src/components/SideBar/SideBar.js
@@ -3,10 +3,13 @@ import ProfileLogo from '../ProfileLogo/ProfileLogo'
 import MyDecksSBTitle from '../MyDecksSBTitle/MyDecksSBTitle'
 import AddNewCard from '../AddNewCard/AddNewCard';
 import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const SideBar = ({page}) => {
 
-  const { deckId } = useParams()
+  const { deckId } = useParams();
+  const userDecks = useSelector(state => state.userDecks.decks);
+  
 
   return (
     <div id="sidebar-div">
